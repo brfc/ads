17358028278a6bdc69149d964cfd8a826106c352
brfc <brfc.email@gmail.com>
2024-04-19 18:00:31 +0100

    format: consistent source text format

--- include/segment-tree.hpp
+++ include/segment-tree.hpp
@@ -13,79 +13,78 @@
 #pragma once
 #include <array>
 
-namespace ads
+namespace ads {
+
+template <typename T, size_t N, typename Functor>
+class SegmentTree
 {
+private:
+    std::array<T, 4 * N> tree;
+    Functor aggregator;
 
-    template <typename T, size_t N, typename Functor>
-    class SegmentTree
+public:
+    SegmentTree() = delete;
+
+    void construct(const std::array<int, N> &arr,
+                   unsigned int node,
+                   unsigned int start,
+                   unsigned int end) noexcept
     {
-    private:
-        std::array<T, 4 * N> tree;
-        Functor aggregator;
-
-    public:
-        SegmentTree() = delete;
-
-        void construct(const std::array<int, N> &arr,
-                       unsigned int node,
-                       unsigned int start,
-                       unsigned int end) noexcept
+        if (start == end)
+            tree[node] = arr[start];
+        else
         {
-            if (start == end)
-                tree[node] = arr[start];
-            else
-            {
-                auto mid_index = (start + end) / 2;
-                auto left_child = 2 * node;
-                auto right_child = 2 * node + 1;
+            auto mid_index = (start + end) / 2;
+            auto left_child = 2 * node;
+            auto right_child = 2 * node + 1;
 
-                construct(arr, left_child, start, mid_index);
-                construct(arr, right_child, mid_index + 1, end);
+            construct(arr, left_child, start, mid_index);
+            construct(arr, right_child, mid_index + 1, end);
 
-                auto value_node = aggregator(tree[left_child], tree[right_child]);
-                tree[node] = value_node;
-            }
+            auto value_node = aggregator(tree[left_child], tree[right_child]);
+            tree[node] = value_node;
         }
+    }
 
-        SegmentTree(std::array<int, N> &arr, Functor lambda) : tree(),
-                                                               aggregator(lambda)
-        {
-            this->construct(arr, 1, 0, N - 1);
-        };
-
-        T get_value(
-            unsigned int node_index,
-            unsigned int start,
-            unsigned int end,
-            unsigned int from,
-            unsigned int to)
-        {
-            auto mid = (start + end) / 2;
-            auto left_child = 2 * node_index;
-            auto right_child = 2 * node_index + 1;
-
-            if (from == start && end == to)
-                return tree[node_index];
-
-            if (from > mid)
-                return get_value(right_child, mid + 1, end, from, to);
-
-            if (to <= mid)
-                return get_value(left_child, start, mid, from, to);
-
-            T left_value = get_value(left_child, start, mid, from, mid);
-            T right_value = get_value(right_child, mid + 1, end, mid + 1, to);
-            return aggregator(left_value, right_value);
-        };
-
-        T get_value(unsigned int from, unsigned int to)
-        {
-            if (from > to)
-            {
-                throw std::invalid_argument("Input values not allowed in get_value function (from > to).");
-            }
-            return get_value(1, 0, N - 1, from, to);
-        }
+    SegmentTree(std::array<int, N> &arr, Functor lambda) : tree(),
+                                                           aggregator(lambda)
+    {
+        this->construct(arr, 1, 0, N - 1);
     };
+
+    T get_value(
+        unsigned int node_index,
+        unsigned int start,
+        unsigned int end,
+        unsigned int from,
+        unsigned int to)
+    {
+        auto mid = (start + end) / 2;
+        auto left_child = 2 * node_index;
+        auto right_child = 2 * node_index + 1;
+
+        if (from == start && end == to)
+            return tree[node_index];
+
+        if (from > mid)
+            return get_value(right_child, mid + 1, end, from, to);
+
+        if (to <= mid)
+            return get_value(left_child, start, mid, from, to);
+
+        T left_value = get_value(left_child, start, mid, from, mid);
+        T right_value = get_value(right_child, mid + 1, end, mid + 1, to);
+        return aggregator(left_value, right_value);
+    };
+
+    T get_value(unsigned int from, unsigned int to)
+    {
+        if (from > to)
+        {
+            throw std::invalid_argument("Input values not allowed in get_value function (from > to).");
+        }
+        return get_value(1, 0, N - 1, from, to);
+    }
+};
 
 } // namespace ads
--- include/trie.hpp
+++ include/trie.hpp
@@ -16,117 +16,116 @@
 #include <memory>
 #include <list>
 
-namespace ads
+namespace ads {
+  
+struct Node
+{
+  std::unordered_map<char, std::unique_ptr<Node>> children;
+  bool is_word;
+
+  Node(bool is_last_char) : children(),
+                            is_word(is_last_char){};
+};
+
+class Trie
 {
 
-  struct Node
+  typedef std::unique_ptr<Node> P_Node;
+
+private:
+  P_Node _root;
+
+  P_Node _insert(P_Node &r, std::string &str) noexcept
   {
-    std::unordered_map<char, std::unique_ptr<Node>> children;
-    bool is_word;
+    if (str.empty())
+      return std::move(r);
+    auto head = str.at(0);
+    auto tail = str.substr(1);
+    auto is_word = tail.empty();
 
-    Node(bool is_last_char) : children(),
-                              is_word(is_last_char){};
-  };
-
-  class Trie
-  {
-
-    typedef std::unique_ptr<Node> P_Node;
-
-  private:
-    P_Node _root;
-
-    P_Node _insert(P_Node &r, std::string &str) noexcept
+    if (r->children.find(head) != r->children.end())
     {
-      if (str.empty())
-        return std::move(r);
-      auto head = str.at(0);
-      auto tail = str.substr(1);
-      auto is_word = tail.empty();
-
-      if (r->children.find(head) != r->children.end())
-      {
-        if (is_word)
-          r->children.at(head)->is_word = is_word;
-        else
-          r->children[head] = _insert(r->children.at(head), tail);
-      }
+      if (is_word)
+        r->children.at(head)->is_word = is_word;
+      else
+        r->children[head] = _insert(r->children.at(head), tail);
+    }
+    else
+    {
+      if (is_word)
+        r->children[head] = std::make_unique<Node>(is_word);
       else
       {
-        if (is_word)
-          r->children[head] = std::make_unique<Node>(is_word);
-        else
-        {
-          auto new_node = std::make_unique<Node>(false);
-          r->children[head] = _insert(new_node, tail);
-        }
+        auto new_node = std::make_unique<Node>(false);
+        r->children[head] = _insert(new_node, tail);
       }
+    }
 
-      return std::move(r);
-    };
-
-    bool _search(const P_Node &r, const std::string &str) const noexcept
-    {
-      if (str.empty())
-        return r->is_word;
-      auto head = str.at(0);
-      auto tail = str.substr(1);
-      return (r->children.find(head) != r->children.end()) && _search(r->children.at(head), tail);
-    };
-
-    std::list<std::string> _suffixes(const std::string &str, const P_Node &r) const
-    {
-      std::list<std::string> return_list;
-
-      for (const auto &el : r->children)
-      {
-        std::string new_str = str + el.first;
-        if (el.second->is_word)
-          return_list.push_back(std::string(new_str));
-
-        std::list<std::string> children_sufixes_list = _suffixes(new_str, el.second);
-        return_list.merge(children_sufixes_list);
-      }
-      return return_list;
-    };
-
-  public:
-    Trie() : _root(std::make_unique<Node>(false)){};
-
-    void insert(const std::string &str)
-    {
-      auto mutable_string = str;
-      if (mutable_string.empty())
-        return;
-      _root = _insert(_root, mutable_string);
-    };
-
-    bool search(const std::string &str) const
-    {
-      return _search(_root, str);
-    };
-
-    std::list<std::string> prefix(const std::string &str) const
-    {
-      Node *pointer = _root.get();
-      std::unique_ptr<Node> *last_reference = nullptr;
-      for (auto c : str)
-      {
-        auto it = pointer->children.find(c);
-        if (it == pointer->children.end())
-          break;
-        pointer = it->second.get();
-        last_reference = &(it->second);
-      }
-
-      std::list<std::string> return_list;
-      if (pointer->is_word)
-        return_list.push_back(str);
-
-      return_list.merge(_suffixes(str, *last_reference));
-
-      return return_list;
-    };
+    return std::move(r);
   };
+
+  bool _search(const P_Node &r, const std::string &str) const noexcept
+  {
+    if (str.empty())
+      return r->is_word;
+    auto head = str.at(0);
+    auto tail = str.substr(1);
+    return (r->children.find(head) != r->children.end()) && _search(r->children.at(head), tail);
+  };
+
+  std::list<std::string> _suffixes(const std::string &str, const P_Node &r) const
+  {
+    std::list<std::string> return_list;
+
+    for (const auto &el : r->children)
+    {
+      std::string new_str = str + el.first;
+      if (el.second->is_word)
+        return_list.push_back(std::string(new_str));
+
+      std::list<std::string> children_sufixes_list = _suffixes(new_str, el.second);
+      return_list.merge(children_sufixes_list);
+    }
+    return return_list;
+  };
+
+public:
+  Trie() : _root(std::make_unique<Node>(false)){};
+
+  void insert(const std::string &str)
+  {
+    auto mutable_string = str;
+    if (mutable_string.empty())
+      return;
+    _root = _insert(_root, mutable_string);
+  };
+
+  bool search(const std::string &str) const
+  {
+    return _search(_root, str);
+  };
+
+  std::list<std::string> prefix(const std::string &str) const
+  {
+    Node *pointer = _root.get();
+    std::unique_ptr<Node> *last_reference = nullptr;
+    for (auto c : str)
+    {
+      auto it = pointer->children.find(c);
+      if (it == pointer->children.end())
+        break;
+      pointer = it->second.get();
+      last_reference = &(it->second);
+    }
+
+    std::list<std::string> return_list;
+    if (pointer->is_word)
+      return_list.push_back(str);
+
+    return_list.merge(_suffixes(str, *last_reference));
+
+    return return_list;
+  };
+};
 
 } // namespace ads
